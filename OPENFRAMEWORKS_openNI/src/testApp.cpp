d569827dd797a43fcd180064821e636c466fe35f
ludimation <mail@ludimotion.com>
2014-10-09 07:22:33 -0400

    Cleaned up "TODO" comments and moved useful function notes to bottom of header file to get them out of the way.

--- OPENFRAMEWORKS_openNI/src/testApp.cpp
+++ OPENFRAMEWORKS_openNI/src/testApp.cpp
@@ -8,156 +8,25 @@ public:
     }
 };
 
-// Commandline application that does ONI to AVI conversion which would be perfect for this project - http://kirilllykov.github.io/blog/2013/03/19/convert-openni-star-dot-oni-files-into-avi/
-// Other possible sources for info 
-// - http://stackoverflow.com/questions/tagged/kinect?page=2&sort=newest&pagesize=15
-// - http://stackoverflow.com/questions/23467476/kinect-skeleton-algorithm-from-depth-images?answertab=active#tab-top
-
-//-- functions and properties which might be useful for recording skeletal data
-//////////////////////
-// ofxOpenNITypes.cpp
-//////////////////////
-//    void ofxOpenNIUser::drawSkeleton() {
-//        ofPushStyle();
-//        // DON'T NEED TO DRAW LIMBS ANYMORE!
-//        //	for(int i = 0; i < limbs.size(); i++){
-//        //		limbs[i].draw();
-//        //	}
-//        for(int i = 0; i < joints.size(); i++){
-//            joints[i].draw();
-//        }
-//        ofPopStyle();
-//    }
-//
-//    ofPixels & ofxOpenNIDepthThreshold::getDepthPixels(){
-//        return depthPixels;
-//    }
-//
-//    ofPixels & ofxOpenNIDepthThreshold::getMaskPixels(){
-//        return maskPixels;
-//    }
-//
-//////////////////////
-// ofxOpenNI.cpp
-//////////////////////
-//
-//
-//    ofPixels& ofxOpenNI::getDepthPixels();
-//    ofPixels& ofxOpenNI::getImagePixels();
-//    xn::UserGenerator& ofxOpenNI::getUserGenerator();
-//    drawSkeletons();
-//    if(g_bIsDepthOn) drawDepth();
-//    if(g_bIsHandsOn) drawHands();
-//    if(g_bIsUserOn) drawSkeletons();
-//    ofTranslate(getWidth(), 0.0f);
-//    if(g_bIsImageOn || g_bIsInfraOn) drawImage();
-// 	currentTrackedUsers[currentTrackedUserIDs[nID]].drawSkeleton();
-//
-//////////////////////
-// ofxOpenNI.h
-//////////////////////
-//    bool setSkeletonProfile(XnSkeletonProfile profile);
-//    XnSkeletonProfile getSkeletonProfile();
-//    xn::UserGenerator& getUserGenerator();
-//    xn::DepthGenerator& getDepthGenerator();
-//    xn::ImageGenerator& getImageGenerator();
-//    xn::DepthMetaData& getDepthMetaData();
-//    xn::ImageMetaData& getImageMetaData();
-//    ofEvent<ofxOpenNIUserEvent> userEvent;
-//
-//////////////////////
-// ofxOpenNiUtils.h
-//////////////////////
-//    enum Joint
-//    enum Limb
-//    static inline ofPoint g_worldToProjective(const ofPoint& p)
-//    static inline ofPoint g_projectiveToWorld(const ofPoint& p)
-//    static inline ofPoint worldToProjective(const XnVector3D& p, xn::DepthGenerator & g_Depth)
-//    inline Joint XnToOfJoint(XnSkeletonJoint type)
-//    inline string getXNJointAsString(XnSkeletonJoint type)
-//    inline string getJointAsString(Joint type)
-//    inline string getLimbAsString(Limb type)
-//    inline string getUserStatusAsString(UserStatusType type)
-//    inline string getCalibrationStatusAsString(XnCalibrationStatus type)
-//
-//////////////////////
-// ofxOpenNiTypes.h
-//////////////////////
-// class ofxOpenNIJoint
-//      ofPoint & getProjectivePosition()
-//      ofPoint & getWorldPosition()
-// class ofxOpenNILimb
-//    void draw() {
-//
-//        ofPushStyle();
-//        if(isFound()){
-//            // draw in green
-//            ofSetColor(0, 255, 0);
-//        }else{
-//            // draw in red
-//            ofSetColor(255, 0, 0);
-//        }
-//        ofSetLineWidth(5);
-//        ofLine(ofVec2f(startJoint->getProjectivePosition()),ofVec2f(endJoint->getProjectivePosition()));
-//        ofPopStyle();
-//    }
-// inline int numJointsInside(ofxOpenNIUser & user)
-//
-////////////////////////
-// ofUtils -- 
-////////////////////////
-// ofGetDay()
-// ofGetFrameNum()
-// ofGetHours()
-// ofGetMinutes()
-// ofGetMonth()
-// ofGetSeconds()
-// ofGetSystemTimeMicros()
-// ofGetYear()
-// ofToString() // "the number is " + ofToString(10)
-//////////////
-// ofLog -- http://www.openframeworks.cc/documentation/utils/ofLog.html
-//////////////
-//class methods
-//    ofLog()
-//    setAutoSpace()
-//    setChannel()
-//global functions
-//    ofGetLogLevel()
-//    ofGetLogLevelName()
-//    ofLogToConsole()
-//    ofLogToFile()
-//    ofSetLogLevel()
-//    ofSetLoggerChannel()
-///////////////
-//
-//    int	getNumTrackedUsers();
-//    xn::UserGenerator& getUserGenerator();
-//    xn::IRMetaData& getIRMetaData(); // ???
-//    ofxOpenNIJoint & getJoint(Joint joint);
-//    ofPoint & getCenter(); // origin/root location?
-///* ofxOpenNIUser(const ofxOpenNIUser& other){
-// if(this != &other){
-// 
-// 
-// setup();
-// center = other.center;
-// joints = other.joints;
-// limbs = other.limbs;
-// */
-//
-// vector<ofxOpenNIJoint> joints;
-//
-///*    ofxOpenNIUser& operator=(const ofxOpenNIUser& other){
-// if(this != &other){
-// 
-// clear();
-// setup();
-// 
-// center = other.center;
-// joints = other.joints;
-// limbs = other.limbs;
-// */
+///////////
+// TODO:
+////////////
+//  - set labels of entries in DB to match loaded image names
+//  - display data associated with current image
+//  - translation, scale, and rotation controls for current image
+//  - relative joint positions // haven't done these yet. Are they necessary?
+//  - joint rotations (axis-angles?)
+//  - L1 norm
+//  - L2 norm
+//  - filters for file tags (drawing, photo, male, female, dancer, pedestrian, public figure, etc.)
+//  - save image positioning data
+//      > rotation
+//      > scale (might need to just store corners so that image can be scaled independent of file size changes
+//  - clean out old oni recording functionality? Doesn't seem useful at this point. Could write a separate app for recording and include bone data for testing input.
+//  - optimize image loading/display by creating an image buffer
+//  - query for short list of potential images (instead of returning just one as we do now)
+//  - loop through list performing bone-by-bone comparison?
+//  - calculate image positions, scales, and rotations based on user locations
 
 
 //--------------------------------------------------------------
@@ -220,7 +89,6 @@ void testApp::setup() {
             images.back().loadImage(filePath);
         }
         
-        // TODO: set indexes of entries in DB to match loaded images
     }
     else printf("Could not find \"images\" directory\n");
 
@@ -253,25 +121,22 @@ void testApp::update(){
         trackedUserJointsDouble.push_back(trackedUserJoints[i][1]);
         trackedUserJointsDouble.push_back(trackedUserJoints[i][2]);
     }
-
-    // TODO: create vector of data for image(s) to be displayed
-    // build data associated with current tracked joint positions (L1 or L2 norm) to use for query
-    // query for short list of potential images
-    // loop through list performing bone-by-bone comparison?
-    // calculate image positions, scales, and rotations based on user locations
 }
 
 //--------------------------------------------------------------
 void testApp::draw(){
 
     ofImage img;
-    
-    ofPushStyle();
-    ofSetColor(255, 255, 255);
 
     switch (displayState) {
         case 'i':
-            //TODO: display graphics in "installation" mode
+            
+            // manage style and drawing matrix
+            ofPushStyle();
+            ofPushMatrix();
+            ofSetColor(255, 255, 255); // white drawing color
+            ofSetBackgroundColor(255, 245, 235); // light tan BG
+            
             // draw image(s)
             if (svm.predict(trackedUserJointsDouble))
             {
@@ -369,15 +234,6 @@ void testApp::keyPressed(int key){
             openNIPlayer.startPlayer(fileName);
             break;
         case 'b':
-            // TODO:  build & display data to associate with currently displayed image
-            //          - joint positions (0–15)
-            //          - relative joint positions
-            //          - joint rotations (axis-angles?)
-            //          - L1 norm
-            //          - L2 norm
-            //          - image positioning data
-            //              > rotation
-            //              > scale (might need to just store corners so that image can be scaled independent of file size changes
             break;
         case 's':
             if (displayState == 'd'){
--- OPENFRAMEWORKS_openNI/src/testApp.h
+++ OPENFRAMEWORKS_openNI/src/testApp.h
@@ -52,3 +52,155 @@ private:
 };
 
 #endif
+
+
+// Commandline application that does ONI to AVI conversion which would be perfect for this project - http://kirilllykov.github.io/blog/2013/03/19/convert-openni-star-dot-oni-files-into-avi/
+// Other possible sources for info
+// - http://stackoverflow.com/questions/tagged/kinect?page=2&sort=newest&pagesize=15
+// - http://stackoverflow.com/questions/23467476/kinect-skeleton-algorithm-from-depth-images?answertab=active#tab-top
+
+//-- functions and properties which might be useful for recording skeletal data
+//////////////////////
+// ofxOpenNITypes.cpp
+//////////////////////
+//    void ofxOpenNIUser::drawSkeleton() {
+//        ofPushStyle();
+//        // DON'T NEED TO DRAW LIMBS ANYMORE!
+//        //	for(int i = 0; i < limbs.size(); i++){
+//        //		limbs[i].draw();
+//        //	}
+//        for(int i = 0; i < joints.size(); i++){
+//            joints[i].draw();
+//        }
+//        ofPopStyle();
+//    }
+//
+//    ofPixels & ofxOpenNIDepthThreshold::getDepthPixels(){
+//        return depthPixels;
+//    }
+//
+//    ofPixels & ofxOpenNIDepthThreshold::getMaskPixels(){
+//        return maskPixels;
+//    }
+//
+//////////////////////
+// ofxOpenNI.cpp
+//////////////////////
+//
+//
+//    ofPixels& ofxOpenNI::getDepthPixels();
+//    ofPixels& ofxOpenNI::getImagePixels();
+//    xn::UserGenerator& ofxOpenNI::getUserGenerator();
+//    drawSkeletons();
+//    if(g_bIsDepthOn) drawDepth();
+//    if(g_bIsHandsOn) drawHands();
+//    if(g_bIsUserOn) drawSkeletons();
+//    ofTranslate(getWidth(), 0.0f);
+//    if(g_bIsImageOn || g_bIsInfraOn) drawImage();
+// 	currentTrackedUsers[currentTrackedUserIDs[nID]].drawSkeleton();
+//
+//////////////////////
+// ofxOpenNI.h
+//////////////////////
+//    bool setSkeletonProfile(XnSkeletonProfile profile);
+//    XnSkeletonProfile getSkeletonProfile();
+//    xn::UserGenerator& getUserGenerator();
+//    xn::DepthGenerator& getDepthGenerator();
+//    xn::ImageGenerator& getImageGenerator();
+//    xn::DepthMetaData& getDepthMetaData();
+//    xn::ImageMetaData& getImageMetaData();
+//    ofEvent<ofxOpenNIUserEvent> userEvent;
+//
+//////////////////////
+// ofxOpenNiUtils.h
+//////////////////////
+//    enum Joint
+//    enum Limb
+//    static inline ofPoint g_worldToProjective(const ofPoint& p)
+//    static inline ofPoint g_projectiveToWorld(const ofPoint& p)
+//    static inline ofPoint worldToProjective(const XnVector3D& p, xn::DepthGenerator & g_Depth)
+//    inline Joint XnToOfJoint(XnSkeletonJoint type)
+//    inline string getXNJointAsString(XnSkeletonJoint type)
+//    inline string getJointAsString(Joint type)
+//    inline string getLimbAsString(Limb type)
+//    inline string getUserStatusAsString(UserStatusType type)
+//    inline string getCalibrationStatusAsString(XnCalibrationStatus type)
+//
+//////////////////////
+// ofxOpenNiTypes.h
+//////////////////////
+// class ofxOpenNIJoint
+//      ofPoint & getProjectivePosition()
+//      ofPoint & getWorldPosition()
+// class ofxOpenNILimb
+//    void draw() {
+//
+//        ofPushStyle();
+//        if(isFound()){
+//            // draw in green
+//            ofSetColor(0, 255, 0);
+//        }else{
+//            // draw in red
+//            ofSetColor(255, 0, 0);
+//        }
+//        ofSetLineWidth(5);
+//        ofLine(ofVec2f(startJoint->getProjectivePosition()),ofVec2f(endJoint->getProjectivePosition()));
+//        ofPopStyle();
+//    }
+// inline int numJointsInside(ofxOpenNIUser & user)
+//
+////////////////////////
+// ofUtils --
+////////////////////////
+// ofGetDay()
+// ofGetFrameNum()
+// ofGetHours()
+// ofGetMinutes()
+// ofGetMonth()
+// ofGetSeconds()
+// ofGetSystemTimeMicros()
+// ofGetYear()
+// ofToString() // "the number is " + ofToString(10)
+//////////////
+// ofLog -- http://www.openframeworks.cc/documentation/utils/ofLog.html
+//////////////
+//class methods
+//    ofLog()
+//    setAutoSpace()
+//    setChannel()
+//global functions
+//    ofGetLogLevel()
+//    ofGetLogLevelName()
+//    ofLogToConsole()
+//    ofLogToFile()
+//    ofSetLogLevel()
+//    ofSetLoggerChannel()
+///////////////
+//
+//    int	getNumTrackedUsers();
+//    xn::UserGenerator& getUserGenerator();
+//    xn::IRMetaData& getIRMetaData(); // ???
+//    ofxOpenNIJoint & getJoint(Joint joint);
+//    ofPoint & getCenter(); // origin/root location?
+///* ofxOpenNIUser(const ofxOpenNIUser& other){
+// if(this != &other){
+//
+//
+// setup();
+// center = other.center;
+// joints = other.joints;
+// limbs = other.limbs;
+// */
+//
+// vector<ofxOpenNIJoint> joints;
+//
+///*    ofxOpenNIUser& operator=(const ofxOpenNIUser& other){
+// if(this != &other){
+//
+// clear();
+// setup();
+//
+// center = other.center;
+// joints = other.joints;
+// limbs = other.limbs;
+// */
