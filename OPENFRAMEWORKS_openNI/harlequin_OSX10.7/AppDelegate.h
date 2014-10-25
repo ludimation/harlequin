b148f7983b4eb7ba1f33d498660c9c59f8cd0bac
ludimation <mail@ludimotion.com>
2014-10-25 10:47:45 -0400

    Pittiful attempt at making an OSX 10.7 target.

---
+++ OPENFRAMEWORKS_openNI/harlequin_OSX10.7/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  harlequin_OSX10.7
+//
+//  Created by david allen on 10/25/14.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ OPENFRAMEWORKS_openNI/harlequin_OSX10.7/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  harlequin_OSX10.7
+//
+//  Created by david allen on 10/25/14.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
