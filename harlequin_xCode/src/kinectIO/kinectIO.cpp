4d660b57a524117dee3d7e92e341ba62f8946e76
david allen <mail@ludimotion.com>
2015-04-22 21:59:53 -0400

    kinectIO::setupTestUserJoints() -- reformatted for legibility

--- harlequin_xCode/src/kinectIO/kinectIO.cpp
+++ harlequin_xCode/src/kinectIO/kinectIO.cpp
@@ -66,64 +66,64 @@ bool kinectIO::stopKinects() {
 }
 
 vector< vector<ofPoint> > kinectIO::setupTestUserJoints() { //TODO: make setting test user joints data more data-driven––load an external XML file or atual positions from a GRT joint data file
-    vector< ofPoint > tempSingleUserJoints;
-    vector< vector <ofPoint> > testUserJoints;
+    vector< ofPoint > joints;
+    vector< vector <ofPoint> > jointSets;
     
     // first user pose //
-    tempSingleUserJoints.push_back(ofPoint(    3.44307  ,   240.111  ,  138.728    ));
-    tempSingleUserJoints.push_back(ofPoint(  -13.2565   ,   429.373  ,  192.71     ));
-    tempSingleUserJoints.push_back(ofPoint(  -24.0613   ,   621.484  ,  261.898    ));
-    tempSingleUserJoints.push_back(ofPoint(   77.3954   ,   472.38   ,   69.9734   ));
-    tempSingleUserJoints.push_back(ofPoint(  297.974    ,   561.992  ,  -55.9768   ));
-    tempSingleUserJoints.push_back(ofPoint(  311.435    ,   845.369  ,   38.7029   ));
-    tempSingleUserJoints.push_back(ofPoint( -103.908    ,   386.367  ,  315.448    ));
-    tempSingleUserJoints.push_back(ofPoint( -197.592    ,   409.68   ,  654.682    ));
-    tempSingleUserJoints.push_back(ofPoint( -211.142    ,   682.998  ,  768.707    ));
-    tempSingleUserJoints.push_back(ofPoint(   74.8465   ,    76.8003 ,   10.6802   ));
-    tempSingleUserJoints.push_back(ofPoint(   15.6446   ,  -316.114  , -112.928    ));
-    tempSingleUserJoints.push_back(ofPoint(    7.12648  ,  -683.95   , -240.483    ));
-    tempSingleUserJoints.push_back(ofPoint(  -34.5611   ,    24.8962 ,  158.811    ));
-    tempSingleUserJoints.push_back(ofPoint(  -96.6418   ,  -371.297  ,   47.5833   ));
-    tempSingleUserJoints.push_back(ofPoint( -147.889    ,  -748.943  ,  -32.6917   ));
-    testUserJoints.push_back(tempSingleUserJoints);
-    tempSingleUserJoints.clear();
+    joints.push_back(ofPoint(    3.44307  ,   240.111  ,  138.728    ));
+    joints.push_back(ofPoint(  -13.2565   ,   429.373  ,  192.71     ));
+    joints.push_back(ofPoint(  -24.0613   ,   621.484  ,  261.898    ));
+    joints.push_back(ofPoint(   77.3954   ,   472.38   ,   69.9734   ));
+    joints.push_back(ofPoint(  297.974    ,   561.992  ,  -55.9768   ));
+    joints.push_back(ofPoint(  311.435    ,   845.369  ,   38.7029   ));
+    joints.push_back(ofPoint( -103.908    ,   386.367  ,  315.448    ));
+    joints.push_back(ofPoint( -197.592    ,   409.68   ,  654.682    ));
+    joints.push_back(ofPoint( -211.142    ,   682.998  ,  768.707    ));
+    joints.push_back(ofPoint(   74.8465   ,    76.8003 ,   10.6802   ));
+    joints.push_back(ofPoint(   15.6446   ,  -316.114  , -112.928    ));
+    joints.push_back(ofPoint(    7.12648  ,  -683.95   , -240.483    ));
+    joints.push_back(ofPoint(  -34.5611   ,    24.8962 ,  158.811    ));
+    joints.push_back(ofPoint(  -96.6418   ,  -371.297  ,   47.5833   ));
+    joints.push_back(ofPoint( -147.889    ,  -748.943  ,  -32.6917   ));
+    jointSets.push_back(joints);
+    joints.clear();
     
     // second test user pose //
-    tempSingleUserJoints.push_back(ofPoint(-130.336    ,    246.013     ,   101.129     ));
-    tempSingleUserJoints.push_back(ofPoint(-155.394    ,    434.483     ,   166.061     ));
-    tempSingleUserJoints.push_back(ofPoint(-180.079    ,    625.777     ,   241.777     ));
-    tempSingleUserJoints.push_back(ofPoint(-281.048    ,    385.199     ,   260.62      ));
-    tempSingleUserJoints.push_back(ofPoint(-519.791    ,    186.235     ,   210.358     ));
-    tempSingleUserJoints.push_back(ofPoint(-797.064    ,    161.881     ,    83.4888    ));
-    tempSingleUserJoints.push_back(ofPoint( -29.7404   ,    483.767     ,    71.501     ));
-    tempSingleUserJoints.push_back(ofPoint( 230.731    ,    417.279     ,  -129.414     ));
-    tempSingleUserJoints.push_back(ofPoint( 467.356    ,    500.446     ,  -311.709     ));
-    tempSingleUserJoints.push_back(ofPoint(-178.519    ,     28.816     ,    91.3145    ));
-    tempSingleUserJoints.push_back(ofPoint( 156.576    ,   -411.119     ,    86.2422    ));
-    tempSingleUserJoints.push_back(ofPoint( 517.507    ,   -391.487     ,   -73.8542    ));
-    tempSingleUserJoints.push_back(ofPoint( -32.0368   ,     86.2691    ,   -18.9189    ));
-    tempSingleUserJoints.push_back(ofPoint( -76.0083   ,   -319.293     ,   -83.0317    ));
-    tempSingleUserJoints.push_back(ofPoint( -10.8147   ,   -708.284     ,  -110.032     ));
-    testUserJoints.push_back(tempSingleUserJoints);
-    tempSingleUserJoints.clear();
+    joints.push_back(ofPoint(-130.336    ,    246.013     ,   101.129     ));
+    joints.push_back(ofPoint(-155.394    ,    434.483     ,   166.061     ));
+    joints.push_back(ofPoint(-180.079    ,    625.777     ,   241.777     ));
+    joints.push_back(ofPoint(-281.048    ,    385.199     ,   260.62      ));
+    joints.push_back(ofPoint(-519.791    ,    186.235     ,   210.358     ));
+    joints.push_back(ofPoint(-797.064    ,    161.881     ,    83.4888    ));
+    joints.push_back(ofPoint( -29.7404   ,    483.767     ,    71.501     ));
+    joints.push_back(ofPoint( 230.731    ,    417.279     ,  -129.414     ));
+    joints.push_back(ofPoint( 467.356    ,    500.446     ,  -311.709     ));
+    joints.push_back(ofPoint(-178.519    ,     28.816     ,    91.3145    ));
+    joints.push_back(ofPoint( 156.576    ,   -411.119     ,    86.2422    ));
+    joints.push_back(ofPoint( 517.507    ,   -391.487     ,   -73.8542    ));
+    joints.push_back(ofPoint( -32.0368   ,     86.2691    ,   -18.9189    ));
+    joints.push_back(ofPoint( -76.0083   ,   -319.293     ,   -83.0317    ));
+    joints.push_back(ofPoint( -10.8147   ,   -708.284     ,  -110.032     ));
+    jointSets.push_back(joints);
+    joints.clear();
     
     // third test user pose //
-    tempSingleUserJoints.push_back(ofPoint(     -19.6065   ,   166.533    ,    68.4897     ));
-    tempSingleUserJoints.push_back(ofPoint(    -185.775    ,   278.772    ,    31.0986     ));
-    tempSingleUserJoints.push_back(ofPoint(    -362.205    ,   390.868    ,    -8.01172    ));
-    tempSingleUserJoints.push_back(ofPoint(    -279.693    ,   154.538    ,    75.5608     ));
-    tempSingleUserJoints.push_back(ofPoint(    -278.583    ,  -108.422    ,    59.2761     ));
-    tempSingleUserJoints.push_back(ofPoint(    -163.701    ,  -100.122    ,  -189.903      ));
-    tempSingleUserJoints.push_back(ofPoint(     -91.8562   ,   403.005    ,   -13.3635     ));
-    tempSingleUserJoints.push_back(ofPoint(     167.173    ,   480.856    ,    24.5776     ));
-    tempSingleUserJoints.push_back(ofPoint(     264.332    ,   254.25     ,   -97.1309     ));
-    tempSingleUserJoints.push_back(ofPoint(      90.3639   ,   -20.0428   ,   132.486      ));
-    tempSingleUserJoints.push_back(ofPoint(     151.953    ,  -427.526    ,   172.202      ));
-    tempSingleUserJoints.push_back(ofPoint(     356.617    ,  -749.295    ,   232.936      ));
-    tempSingleUserJoints.push_back(ofPoint(     202.759    ,   128.632    ,    79.2761     ));
-    tempSingleUserJoints.push_back(ofPoint(      93.9081   ,  -249.735    ,   -48.7756     ));
-    tempSingleUserJoints.push_back(ofPoint(     -11.0111   ,  -615.832    ,  -112.657      ));
-    testUserJoints.push_back(tempSingleUserJoints);
+    joints.push_back(ofPoint(     -19.6065   ,   166.533    ,    68.4897     ));
+    joints.push_back(ofPoint(    -185.775    ,   278.772    ,    31.0986     ));
+    joints.push_back(ofPoint(    -362.205    ,   390.868    ,    -8.01172    ));
+    joints.push_back(ofPoint(    -279.693    ,   154.538    ,    75.5608     ));
+    joints.push_back(ofPoint(    -278.583    ,  -108.422    ,    59.2761     ));
+    joints.push_back(ofPoint(    -163.701    ,  -100.122    ,  -189.903      ));
+    joints.push_back(ofPoint(     -91.8562   ,   403.005    ,   -13.3635     ));
+    joints.push_back(ofPoint(     167.173    ,   480.856    ,    24.5776     ));
+    joints.push_back(ofPoint(     264.332    ,   254.25     ,   -97.1309     ));
+    joints.push_back(ofPoint(      90.3639   ,   -20.0428   ,   132.486      ));
+    joints.push_back(ofPoint(     151.953    ,  -427.526    ,   172.202      ));
+    joints.push_back(ofPoint(     356.617    ,  -749.295    ,   232.936      ));
+    joints.push_back(ofPoint(     202.759    ,   128.632    ,    79.2761     ));
+    joints.push_back(ofPoint(      93.9081   ,  -249.735    ,   -48.7756     ));
+    joints.push_back(ofPoint(     -11.0111   ,  -615.832    ,  -112.657      ));
+    jointSets.push_back(joints);
     
-    return testUserJoints;
+    return jointSets;
 }
